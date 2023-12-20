9b9dc197a2b7221b0dd88c1fe72cb80c4bcb77f2
JeefPolos <jeefry.polo@outlook.com>
2023-12-20 18:50:10 -0500

    control de rutas con jwt

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const passport = require('passport');
 
 const CategoryService = require('./../services/category.service');
 const validatorHandler = require('./../middlewares/validator.handler');
 const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
+
 
 const router = express.Router();
 const service = new CategoryService();
@@ -30,6 +32,7 @@ router.get('/:id',
 );
 
 router.post('/',
+passport.authenticate('jwt', {session: false}),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
